cef4250c61864a8a1104cf9b1d25fbc0732a763f
Shawn Tu <1273379047@qq.com>
2025-11-06 13:48:50 +0800

    bug fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode
 }) {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   // 检查用户是否登录
   const {
